0db2c8bfe04a636a45f4214b8a58fe88a1ba8e3d
Akm-Azad <keyazad03@gamail.com>
2024-03-01 01:47:37 +0800

    add js in the button

--- script/index.js
+++ script/index.js
@@ -1,3 +1,16 @@
 document.getElementById("scrollButton").addEventListener("click", function() {
       document.getElementById("scrollSection").scrollIntoView({ behavior: 'smooth' });
-  });
+});
+  
+const buttonContainer = document.querySelector('.btnn-container');
+const buttons = buttonContainer.querySelectorAll('.btn');
+let clickCount = 0; 
+buttons.forEach(button => {
+    button.addEventListener('click', function() {
+        this.style.backgroundColor = 'lime';
+        clickCount++;
+        if (clickCount > 4) {
+            alert('Opps! you can not select more than 4 seats');
+        }
+    });
+});
